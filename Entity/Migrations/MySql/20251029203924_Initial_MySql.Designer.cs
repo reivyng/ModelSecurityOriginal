1c8652c437c8166a152398cf589cd8805701a65d
Reyving <medinafabriany@gmail.com>
2025-10-29 15:39:43 -0500

    ..

--- Entity/Migrations/MySql/20251029203924_Initial_MySql.Designer.cs
+++ Entity/Migrations/MySql/20251029203924_Initial_MySql.Designer.cs
@@ -4,16 +4,19 @@ using Entity.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Entity.Migrations
+namespace Entity.Migrations.MySql
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251029203924_Initial_MySql")]
+    partial class Initial_MySql
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
